ee84922b1cc89884c4f8e756a8d6954d64cfbb9f
ZwCreatePhoton <ZwCreatePhoton@gmail.com>
2021-07-29 08:56:15 -0700

    Fixed javascript comment regex

--- htmlmth/mods/javascript/remove_comments.py
+++ htmlmth/mods/javascript/remove_comments.py
@@ -4,8 +4,8 @@ from . import Mod
 
 
 def remove_comments(output):
-    output = re.sub(r'(\/\*[\w\'\s\n\*]*\*\/)', '', output)  # multi-line comments
-    output = re.sub(r'\/\/[^\r\n]*', '', output)  # single-line comments
+    output = re.sub(r'(\/\*[\w\'\s\n\*]*\*\/)', r'', output)  # multi-line comments
+    output = re.sub(r'((?:[\s;]+)|^)(\/\/.*$)', r'\1', output)  # single-line comments
     return output
 
 mod_remove_comments = Mod(remove_comments)
